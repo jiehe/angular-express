2b310ff743675338b8a2734139a887172f942e45
lijiehe <combine.li@corp.globalmarket.com>
2015-02-13 22:04:35 +0800

    fix bug

--- public/app/js/controller/sync/banktemplate.js
+++ public/app/js/controller/sync/banktemplate.js
@@ -25,16 +25,20 @@ define(['../controllers', 'uploadFile', 'jDialog'], function (controllers, uploa
       time = setInterval(function() {
         obj =$('iframe')[0].contentWindow.document.getElementsByTagName('pre');
         if(obj.length>0) {
-          clearInterval(time);
-          callBackData = JSON.parse(obj[0].innerHTML);
-          if(callBackData.models[0].success) {
-            var url = callBackData.models[0].url;
-            importBankInfo.getNewData({filePath: url},function(data){
-              jDialog.alert(data.message);
-              clearInterval(time);
-            })
-          }else {
-            alert('上传失败'+JSON.stringify(callBackData));
+
+          try{
+            callBackData = JSON.parse(obj[0].innerHTML);
+            if(callBackData.models[0].success) {
+              var url = callBackData.models[0].url;
+              importBankInfo.getNewData({filePath: url},function(data){
+                jDialog.alert(data.message);
+                clearInterval(time);
+              })
+            }else {
+              alert('上传失败'+JSON.stringify(callBackData));
+            }
+          }catch(e){
+            jDialog.alert(e);
           }
 
         }
